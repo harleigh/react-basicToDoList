3de2b408f82ad50fe4c0a2ae7f112f2ad4b0ab6c
harleigh <harleigh.marsh@gmail.com>
2023-09-06 12:19:28 -0700

    data persists when refresh occurs

--- src/todoApp.js
+++ src/todoApp.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import TodoForm from "./TodoForm"
 import TodoList from "./TodoList"
+import { useEffect } from "react"
 
 export default function TodoApp () {
     
@@ -8,7 +9,20 @@ export default function TodoApp () {
      * allTodos are a collection of todos that are objects in the form
      *      {id: str, name: str, isDone:boolean}
      */
-    const [allTodos, setAllTodos] = useState([])
+    const [allTodos, setAllTodos] = useState( () =>{
+        const localVals = localStorage.getItem("ACTIVE_TODOS")
+        if( localVals===null || localVals===undefined){
+            return []
+        }
+        return JSON.parse(localVals)    //puts into an array of todo objects
+    })
+
+    /**
+     * So our data will persist when the refresh key is struck
+     */
+    useEffect( () => {
+        localStorage.setItem("ACTIVE_TODOS", JSON.stringify(allTodos))
+    }, [allTodos])
    
     return (
         <>
