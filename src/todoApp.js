143f7b0743834fdf22f17042f7cb5e0978805765
harleigh <harleigh.marsh@gmail.com>
2023-09-06 09:28:50 -0700

    basic functionality -- need to add delete

--- src/todoApp.js
+++ src/todoApp.js
@@ -1,25 +1,79 @@
-import { useState, useRef } from "react"
+import { useState } from "react"
 
 export default function TodoApp () {
-    const newTodoRef = useRef()
+    const [todoName, setTodoName] = useState("")
+    const [allTodos, setAllTodos] = useState([])
 
+    const toggleTodoCheckbox = (id, currentCheckState) =>{
+        setAllTodos( (currentTodos) => {
+            //return a new collection of the current todo list (where the one with the id is modified)
+            return currentTodos.map( (todo) => {
+                if (todo.id === id) {
+                    const newTodo = {id: todo.id, name: todo.name, isDone: currentCheckState}
+                    return newTodo
+                }
+                return todo
+            })
+        })
+    }
+
+    /**
+     * A todo list item is a component with:
+     *      A Checkbox (keyed on their id)
+     *      The name of the todo item (e.g. wash floor)
+     *      A button to delete the item
+     */
+    const generateTodoListItems = () => {
+        return allTodos.map( (todoItem) => {
+            return(
+              <li key={todoItem.id}>
+                <label>
+                    <input  type="checkbox"
+                            checked={todoItem.isDone}
+                            onChange={(e)=>toggleTodoCheckbox( todoItem.id, e.target.checked)}/>
+                    {todoItem.isDone? <s>{todoItem.name}</s>:todoItem.name}
+                </label>
+                <button className="deleteTodo">Delete</button>
+              </li>  
+            )
+        })
+    }
+
+    const generateNewTodoItem = ()=> {
+        return {id: crypto.randomUUID(),
+                name: todoName,
+                isDone: false}
+    }
 
     const handleFormSubmit = (event) => {
         event.preventDefault()
-        console.log("New todo name is: ", newTodoRef.current.value)
-        
+        setAllTodos( (curentTodos) => {
+            return [ ...curentTodos,
+                     generateNewTodoItem()]
+        })//end set all todos
+
+        setTodoName("")
     }
 
+   
     return (
         <>
-        <form onSubmit={handleFormSubmit}>
-            <label htmlFor="todoItem">Enter New ToDo:</label>
-            <input type="text"
-                   id="todoItem"
-                   placeholder="new todo item..."
-                   ref={newTodoRef}/>
-            <button className="btnTodo">Add</button>
-        </form>
+        <div className="todoApp">
+            <form onSubmit={handleFormSubmit}
+                  autoComplete="off">
+                <label htmlFor="todoItem">Enter New ToDo:</label>
+                <input type="text"
+                    id="todoItem"
+                    placeholder="new todo item..."
+                    onChange={ (e) => setTodoName(e.target.value) }
+                    value={todoName}/>
+                <button className="btnTodo"
+                        disabled={todoName===""} >Add</button>
+            </form>
+            <ul>
+                {generateTodoListItems()}
+            </ul>
+        </div>
         </>
     )
 }
